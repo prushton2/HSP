85c0a70131d62d7e6ded3b10e20a39613f65acb2
prushton2 <peterrushton418@gmail.com>
2026-05-06 14:45:11 -0400

    removed dead code

--- backend/src/repository/mod.rs
+++ backend/src/repository/mod.rs
@@ -2,16 +2,12 @@ pub mod student_repository;
 pub mod auth_repository;
 pub mod activities_repository;
 
-use std::ops::DerefMut;
-
 use axum::async_trait;
 use downcast_rs::{DowncastSync, impl_downcast};
 
 pub use student_repository::StudentRepository;
 pub use auth_repository::AuthRepository;
 pub use activities_repository::ActivitiesRepository;
-
-use crate::database::MockDB;
 
 #[async_trait]
 pub trait Repository: 
