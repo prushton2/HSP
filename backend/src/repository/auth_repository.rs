25e9bab1c9d41217e199a454ccdc842f0f9bfe38
prushton2 <peterrushton418@gmail.com>
2026-05-06 10:46:59 -0400

    refactoring

--- backend/src/repository/auth_repository.rs
+++ backend/src/repository/auth_repository.rs
@@ -8,19 +8,19 @@ use crate::types::Role;
 #[allow(dead_code)]
 #[async_trait]
 pub trait AuthRepository: Send + Sync {
-    async fn insert_user(&self, user: &FullUser) -> Result<(), Error>;
+    async fn insert_user(&self, user: &User) -> Result<(), Error>;
     async fn update_user(&self, uuid: &str, update: &UpdateUser) -> Result<(), Error>;
     async fn delete_user(&self, uuid: &str) -> Result<(), Error>;
-    async fn get_user   (&self, uuid: &str) -> Result<(FullUser, Vec<TokenInfo>), Error>;
-    async fn getall_user(&self) -> Result<Vec<FullUser>, Error>;
+    async fn get_user   (&self, uuid: &str) -> Result<(User, Vec<Token>), Error>;
+    async fn getall_user(&self) -> Result<Vec<User>, Error>;
 
     async fn insert_token(&self, uuid: &str, plain_token: &str, signup_hash: &str, expiry: i64) -> Result<(), Error>;
     async fn update_token(&self, uuid: &str, old_token: &str, new_token: Option<&str>, new_signup_hash: Option<&str>, new_expiry: Option<i64>) -> Result<(), Error>;
     async fn delete_token(&self, uuid: &str, token: &str) -> Result<(), Error>;
-    async fn get_token   (&self, token: &str) -> Result<TokenInfo, Error>; 
-    async fn getall_token(&self) -> Result<Vec<TokenInfo>, Error>;
+    async fn get_token   (&self, token: &str) -> Result<Token, Error>; 
+    async fn getall_token(&self) -> Result<Vec<Token>, Error>;
     
-    async fn get_token_hash(&self, signup_hash: &str) -> Result<TokenInfo, Error>;
+    async fn get_token_hash(&self, signup_hash: &str) -> Result<Token, Error>;
     async fn delete_tokens (&self, uuid: &str) -> Result<(), Error>;
 }
 
@@ -35,7 +35,7 @@ pub trait AuthRepository: Send + Sync {
 
 
 #[derive(Serialize, Deserialize)]
-pub struct TokenInfo {
+pub struct Token {
     pub uuid: String,
     pub token: String,
     pub signup_hash: String,
@@ -43,7 +43,7 @@ pub struct TokenInfo {
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct FullUser {
+pub struct User {
     pub uuid: String,
     pub fname: String,
     pub lname: String,
--- backend/src/repository/student_repository.rs
+++ backend/src/repository/student_repository.rs
@@ -12,17 +12,17 @@ pub trait StudentRepository: Send + Sync {
     async fn get_studentinfo   (&self, uuid: &str) -> Result<StudentInfo, Error>;
     async fn search_studentinfo(&self, params: &SearchStudentInfo) -> Result<Vec<StudentInfo>, Error>;
 
-    async fn insert_encrypted(&self, data: &EncryptedInfo) -> Result<(), Error>;
-    async fn update_encrypted(&self, update: &UpdateEncryptedInfo) -> Result<(), Error>;
+    async fn insert_encrypted(&self, data: &StudentEncrypted) -> Result<(), Error>;
+    async fn update_encrypted(&self, update: &UpdateStudentEncrypted) -> Result<(), Error>;
     async fn delete_encrypted(&self, uuid: &str) -> Result<(), Error>;
-    async fn get_encrypted   (&self, uuid: &str) -> Result<EncryptedInfo, Error>;
-    async fn getall_encrypted(&self) -> Result<Vec<EncryptedInfo>, Error>;
+    async fn get_encrypted   (&self, uuid: &str) -> Result<StudentEncrypted, Error>;
+    async fn getall_encrypted(&self) -> Result<Vec<StudentEncrypted>, Error>;
     
-    async fn insert_residence(&self, user: &ResidenceInfo) -> Result<(), Error>;
-    async fn update_residence(&self, update: &UpdateResidenceInfo) -> Result<(), Error>;
+    async fn insert_residence(&self, user: &StudentResidence) -> Result<(), Error>;
+    async fn update_residence(&self, update: &UpdateStudentResidence) -> Result<(), Error>;
     async fn delete_residence(&self, uuid: &str) -> Result<(), Error>;
-    async fn get_residence   (&self, uuid: &str) -> Result<ResidenceInfo, Error>;
-    async fn search_residence(&self, params: &SearchResidenceInfo) -> Result<Vec<ResidenceInfo>, Error>;
+    async fn get_residence   (&self, uuid: &str) -> Result<StudentResidence, Error>;
+    async fn search_residence(&self, params: &SearchStudentResidence) -> Result<Vec<StudentResidence>, Error>;
 }
 
 #[derive(Serialize, Deserialize)]
@@ -43,30 +43,30 @@ pub struct UpdateStudentInfo {
 pub type SearchStudentInfo = UpdateStudentInfo;
 
 #[derive(Serialize, Deserialize)]
-pub struct EncryptedInfo {
+pub struct StudentEncrypted {
     pub uuid: String,
     pub data: String
 }
 
-pub struct UpdateEncryptedInfo {
+pub struct UpdateStudentEncrypted {
     pub uuid: String,
     pub data: Option<String>
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct ResidenceInfo {
+pub struct StudentResidence {
     pub uuid: String,
     pub hall: String,
     pub room: i32,
     pub wing: String
 }
 
-pub struct UpdateResidenceInfo {
+pub struct UpdateStudentResidence {
     pub uuid: String,
     pub hall: Option<String>,
     pub room: Option<i32>,
     pub wing: Option<String>,
 }
 
-pub type SearchResidenceInfo = UpdateResidenceInfo;
+pub type SearchStudentResidence = UpdateStudentResidence;
 
